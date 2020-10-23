108191e62656d90e736a172a883aa97ec54b5589
mccray austin <tubach75@hotmail.com>
2020-10-23 13:55:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   def execute_transaction
     if sender.balance < self.amount || !sender.valid?
       self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     elsif self.status != "complete" && sender.valid?
       sender.balance -= amount
       receiver.balance += amount
