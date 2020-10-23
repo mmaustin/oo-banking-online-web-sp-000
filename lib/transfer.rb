7514988d91ae2a642f4daedf4c97301c1541135b
mccray austin <tubach75@hotmail.com>
2020-10-23 13:28:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     if sender.balance < self.amount || sender.status == "closed"
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
-    else # self.status != "complete" && sender.valid?
+    elsif self.status != "complete" && sender.valid?
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
